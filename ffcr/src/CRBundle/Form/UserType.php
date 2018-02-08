ca443229a4568d08b03b71893e404dbadfb8bbbd
Amortila Guillaume <guillaume.amortila@eemi.com>
2018-02-08 12:42:37 +0100

    map et form

---
+++ ffcr/src/CRBundle/Form/UserType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CRBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('fname')->add('lname')->add('email')->add('pass')->add('points');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CRBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'crbundle_user';
+    }
+
+
+}
