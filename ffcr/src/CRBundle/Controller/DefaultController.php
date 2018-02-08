9444fa4ead6f17fb48b9e8c530a687f17b92d2b6
Amortila Guillaume <guillaume.amortila@eemi.com>
2018-02-08 19:50:08 +0100

    query

--- ffcr/src/CRBundle/Controller/DefaultController.php
+++ ffcr/src/CRBundle/Controller/DefaultController.php
@@ -9,20 +9,34 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-        $pisteRepo = $em->getRepository('CRBundle:Piste');
+        //$pisteRepo = $em->getRepository('CRBundle:Piste');
 
 
 
-        $pistes = $pisteRepo->findAll(['date' => 'desc']);
+        //$pistes = $pisteRepo->findAll(['date' => 'desc']);
         /*$query = $em->createQuery(
             'SELECT *
-        FROM CRBundle\Entity\Courses p
-        WHERE p.price > :price
-        ORDER BY p.price ASC'
-        )->setParameter('price', 10);
-
+        FROM piste p
+        WHERE p.date > NOW()
+        ORDER BY p.date DESC'
+        );
         // returns an array of Product objects
         $pistes = $query->execute();*/
+
+        $conn = $em->getConnection();
+
+        $sql = '
+        SELECT *
+        FROM piste p
+        WHERE p.date > NOW()
+        ORDER BY p.date DESC
+        ';
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        $pistes = $stmt->fetchAll();
+
+
 
 
         return $this->render('CRBundle:Default:index.html.twig',[
