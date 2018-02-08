b56a51c08ce2ed3c1fc0b178d55ee74f3b49b673
Amortila Guillaume <guillaume.amortila@eemi.com>
2018-02-08 12:59:53 +0100

    form

--- ffcr/src/CRBundle/Form/PisteType.php
+++ ffcr/src/CRBundle/Form/PisteType.php
@@ -2,6 +2,7 @@
 
 namespace CRBundle\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
@@ -15,12 +16,15 @@ class PisteType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('ville', TextType::class, ['attr' => ['placeholder' => 'Ville']])
-            ->add('nom', TextType::class, ['attr' => ['placeholder' => 'Nom de la piste']])
-            ->add('coordx', TextType::class, ['attr' => ['placeholder' => 'Coordonnées ']])
-            ->add('coordy', TextType::class, ['attr' => ['placeholder' => 'Ville']])
-            ->add('cheese', TextType::class, ['attr' => ['placeholder' => 'Ville']])
-            ->add('Soumettre', SubmitType::class, ['attr' => ['placeholder' => 'Ville']]);
+        $builder->add('ville', TextType::class, ['attr' => ['placeholder' => 'Nom de la ville, du lieu *']])
+            ->add('nom', TextType::class, ['attr' => ['placeholder' => 'Nom de la piste *']])
+            ->add('coordx', TextType::class, ['attr' => ['placeholder' => 'Latitude (Coordonnées exactes) *']])
+            ->add('coordy', TextType::class, ['attr' => ['placeholder' => 'Longitude (Coordonnées exactes) *']])
+            ->add('cheese', TextType::class, ['required' => false, 'attr' => ['placeholder' => 'Fromage sponsor']])
+            ->add('level', ChoiceType::class, array(
+                'choices' => array('Blanche' => 0, 'Verte' => 1, 'Bleue' => 2, 'Rouge' => 3, 'Noire' => 4)
+            ))
+            ->add('Soumettre', SubmitType::class, ['attr' => ['class' => 'form-control btn btn-success']]);
 
     }
     
