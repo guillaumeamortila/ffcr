77a5af9e5a2c6d6d516d1076310029621dc5137d
Amortila Guillaume <guillaume.amortila@eemi.com>
2018-02-09 14:44:38 +0100

    homepage

--- ffcr/src/CRBundle/Form/PisteType.php
+++ ffcr/src/CRBundle/Form/PisteType.php
@@ -28,7 +28,7 @@ class PisteType extends AbstractType
             ))
             ->add('description', TextareaType::class, ['label' => false])
             ->add('date', DateType::class, ['label' => false])
-            ->add('Soumettre', SubmitType::class, ['attr' => ['class' => 'form-control btn btn-success']]);
+            ->add('Soumettre', SubmitType::class, ['attr' => ['class' => 'form-control button btn ']]);
 
     }
 
